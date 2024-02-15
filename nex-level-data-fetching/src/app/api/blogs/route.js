5be8260c4f8b8e2d41e0f3ded57f3b4fa9a49b4e
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2024-02-16 01:07:57 +0600

    Basic Idea On API Routes

---
+++ nex-level-data-fetching/src/app/api/blogs/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server"
+
+
+// GET API
+export const GET =  () =>{
+    return NextResponse.json({message: "Hello, World!"})
+}
+
+// POST API
+export const POST = () =>{}
+
+// PATCH API
+export const  PATCH  = () =>{}
+
+// POST API
+export const DELETE = () =>{}
